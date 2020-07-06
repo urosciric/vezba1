474abcc651c4c8f7fbb6235c220d236969037ce4
cira49152 <dusan.ciric@ensaco.rs>
2020-07-06 09:26:57 +0200

    P_robni commit

---
+++ vezba1/taja.cpp
@@ -0,0 +1,8 @@
+#include "taja.h"
+#include <iostream>
+
+
+void function_taja(int vezba1)
+{
+    std::cout << "Poslao si mi :" << vezba1 << "\r\n";
+}
---
+++ vezba1/taja.h
@@ -0,0 +1,4 @@
+#pragma once
+
+
+void function_taja(int vezba1);
