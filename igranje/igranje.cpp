5679d42e60237f7139f98ad5dd9f1a28185e2dd9
urke.ciric@gmail.com <urke.ciric@gmail.com>
2020-07-15 09:31:24 +0200

    igranje

---
+++ igranje/igranje.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <map>
+#include <vector>
+#include <functional>
+#include <string>
+
+int main()
+{
+    std::map<std::string, int>mapa;
+    mapa["zikica"] = 89456894;
+    mapa["mikica"] = 53455794;
+}
