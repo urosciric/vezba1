2f9ec4132564f64c9e3a77feabf57d7ecaa0eb59
urke.ciric@gmail.com <urke.ciric@gmail.com>
2020-12-09 15:30:11 +0100

    uradio tabelu da radi

--- urkeapi/term_table.cpp
+++ urkeapi/term_table.cpp
@@ -5,12 +5,14 @@
 namespace urke
 {
 
-void rx_dump_table(const rx_table_type& table, std::ostream& out, bool column_names, bool dot_lines)
+void rx_dump_table(const rx_table_type& table, std::ostream& out, bool column_names, bool dot_lines, const char table_frame)
 {
 	if (table.empty())
 		return;
 
 	const size_t col_diff = 2;
+
+	const char option = table_frame;
 
 	out << ANSI_CUR(0, 0);
 
@@ -48,24 +50,79 @@ void rx_dump_table(const rx_table_type& table, std::ostream& out, bool column_na
 	char empty_char = dot_lines ? '.' : ' ';
 	// now we have all widths
 	bool first = true;
+	size_t first_line = 0;
+
+	///////
+
+	if (option == '-')
+	{
+		for (size_t i = 0; i < columns_number; i++)
+			first_line += (widths[i] + col_diff);
+
+		out << '+';
+
+		for (size_t i = 1; i < first_line + 3; i++)
+			out << option;
+
+		out << '+' << "\r\n";
+	}
+
+	else
+	{
+		for (size_t i = 0; i < columns_number; i++)
+			first_line += (widths[i] + col_diff);
+
+		for (size_t i = 1; i < first_line + 5; i++)
+			out << ' ';
+
+		out << "\r\n";
+	}
+
+	///////
+
+	int pom = 0;
+
 	for (const auto& row : table)
 	{
 		if (!first)
 			out << "\r\n";
 		if (!row.empty())
 		{
+
+			if (option == '-')
+				out << ANSI_COLOR_GRAY << "|  " << ANSI_COLOR_RESET;
+			else
+				out << "   ";
+
 			for (size_t i = 0; i < columns_number; i++)
 			{
+
 				string_type rest(widths[i] + col_diff - row[i].value.size(),
 					i == columns_number - 1 || first || row[i].value.empty()
 					? ' ' : empty_char);
+
 				if (!row[i].prefix.empty())
 					out << row[i].prefix;
 				out << row[i].value;
 				if (!row[i].postfix.empty())
 					out << row[i].postfix;
-				out << rest;
+				if (i == columns_number - 1 && empty_char == '.')
+				{
+					for (int i = 0; i < rest.size() - 2; i++)
+						out << '.';
+				}
+				else
+				{
+					out << rest;
+				}
+
 			}
+
+			if (option == '-')
+				out << ANSI_COLOR_GRAY << "  |" << ANSI_COLOR_RESET;
+			else  
+				out << "   ";
+
 		}
 		if (first)
 			first = false;
@@ -75,10 +132,39 @@ void rx_dump_table(const rx_table_type& table, std::ostream& out, bool column_na
 			size_t total_width = 0;
 			for (size_t i = 0; i < columns_number; i++)
 				total_width += (widths[i] + col_diff);
-			string_type rest(total_width, '=');
+
+			if (option == '-') out << '+';
+			else out << ' ';
+			string_type rest(total_width + 2, option);
+
 			out << ANSI_COLOR_GRAY << rest << ANSI_COLOR_RESET;
+			if (option == '-') out << '+';
+			else out << ' ';
 			column_names = false;
 		}
+		pom++;
+	}
+
+	out << "\r\n";
+
+	if (option == '-')
+	{
+		out << '+';
+
+		for (size_t i = 1; i < first_line + 3; i++)
+			out << option;
+
+		out << '+' << "\r\n";
+	}
+
+	else
+	{
+			
+		for (size_t i = 0; i < first_line + 4; i++)
+			out << ' ';
+
+		out << "\r\n";
+
 	}
 
 	out << "\r\n" << ANSI_CUR(tabela.size(), 0);
